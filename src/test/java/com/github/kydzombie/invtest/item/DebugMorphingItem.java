0542c58bbbaf49e3cdb7d34cd43cfbf7a66f72b2
Logan Reynolds <gizmordo@gmail.com>
2023-07-18 12:01:28 -0400

    Fixed morphing.

--- src/test/java/com/github/kydzombie/invtest/item/DebugMorphingItem.java
+++ src/test/java/com/github/kydzombie/invtest/item/DebugMorphingItem.java
@@ -1,11 +1,14 @@
 package com.github.kydzombie.invtest.item;
 
+import com.github.kydzombie.extendedinventory.ExtendedInventoryUtil;
 import com.github.kydzombie.extendedinventory.item.TemplateTrinket;
 import com.github.kydzombie.extendedinventory.item.TrinketType;
 import net.minecraft.entity.player.PlayerBase;
 import net.minecraft.item.ItemInstance;
 import net.minecraft.level.Level;
 import net.modificationstation.stationapi.api.registry.Identifier;
+
+import java.util.Arrays;
 
 public class DebugMorphingItem extends TemplateTrinket {
     public DebugMorphingItem(Identifier identifier) {
@@ -14,13 +17,19 @@ public class DebugMorphingItem extends TemplateTrinket {
 
     @Override
     public void onTrinketEquipped(Level level, PlayerBase player, ItemInstance item, int trinketSlot) {
-        System.out.println("Morphed into " + TrinketType.values()[trinketSlot].name());
-        item.getStationNBT().put("invtest:placedInSlotOfType", TrinketType.values()[trinketSlot].ordinal());
+
+        var preferredType = Arrays.stream(ExtendedInventoryUtil.getAcceptedTypes(trinketSlot)).filter(trinketType -> Arrays.stream(getTrinketTypes(item)).anyMatch(otherTrinket -> otherTrinket == trinketType)).findFirst();
+        if (preferredType.isPresent()) {
+            System.out.println("Morphed into " + preferredType.get().name());
+            item.getStationNBT().put("invtest:placedInSlotOfType", preferredType.map(Enum::ordinal).get());
+        } else {
+            System.out.println("Impossible morph");
+        }
     }
 
     @Override
     public void onTrinketUnequipped(Level level, PlayerBase player, ItemInstance item, int trinketSlot) {
         System.out.println("Demorphed");
-        item.getStationNBT().put("invtest:placedInSlotOfType", 0);
+        item.getStationNBT().put("invtest:placedInSlotOfType", -1);
     }
 }
